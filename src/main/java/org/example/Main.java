196c0ab1d44102af9fa82793c853df5afb31002f
Taniya <taniya.sharma@credable.in>
2022-03-02 12:35:56 +0530

    separating firefox and chrome fucntions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,35 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Main {
+
     public static void main(String[] args) {
-       // WebDriver driver = new ChromeDriver();
 
-      FirefoxOptions options = new FirefoxOptions();
-       options.setBinary("/snap/bin/firefox");
+       // WebDriver driver = chromeDriver();
+        WebDriver driver = firefoxDriver();
 
-        WebDriver driver = new FirefoxDriver();
         driver.get("http://google.com");
-
+        // document.getElementsByName("q")[0]
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.click();
-
+        // document.getElementsByName("q")[0].value = "Selenium Testing"
         searchBox.sendKeys("Selenium Testing");
         searchBox.sendKeys(Keys.ENTER);
-
     }
+    public static WebDriver firefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
+    public static WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
 }
