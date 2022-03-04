65e90e20cb6e2fc4d62ccebc7aea975a45c1b37d
Taniya <taniya.sharma@credable.in>
2022-03-04 11:05:53 +0530

    testing 2017 blog

--- src/main/java/org/example/SeleniumWebsiteTest.java
+++ src/main/java/org/example/SeleniumWebsiteTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 
 public class SeleniumWebsiteTest {
 @Test
-    public void test_selenium_dot_dev_blog(){
+    public void test_selenium_dot_dev_blog_2016_page(){
         WebDriver driver = chromeDriver();
         driver.get("https://www.selenium.dev/");
 
@@ -23,6 +23,22 @@ public class SeleniumWebsiteTest {
 
         String title = driver.getTitle();
         assertEquals("Blog Posts - 2016 | Selenium", title);
+
+        driver.quit();
+    }
+
+    public void test_selenium_dot_dev_blog_2017_page(){
+        WebDriver driver = chromeDriver();
+        driver.get("https://www.selenium.dev/");
+
+        WebElement blog =driver.findElement(By.linkText("Blog"));
+        blog.click();
+        WebElement link2017 = driver.findElement(By.id("m-blog2017-li"));
+        link2017.click();
+
+
+        String title = driver.getTitle();
+        assertEquals("Blog Posts - 2017 | Selenium", title);
 
         driver.quit();
     }
