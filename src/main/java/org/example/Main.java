9fcfdab22f4b553f64117671848c9770e5ce3266
Taniya <taniya.sharma@credable.in>
2022-03-04 10:24:51 +0530

    adding dependcy for junit and adding separate functions for testing google and selenium

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,9 +12,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-       // WebDriver driver = chromeDriver();
-        WebDriver driver = firefoxDriver();
+       WebDriver driver = chromeDriver();
+       // WebDriver driver = firefoxDriver();
+//testGoogle(driver);
+testSelenium(driver);
+    }
 
+    public static void testGoogle(WebDriver driver){
         driver.get("http://google.com");
         // document.getElementsByName("q")[0]
         WebElement searchBox = driver.findElement(By.name("q"));
@@ -22,6 +26,15 @@ public class Main {
         // document.getElementsByName("q")[0].value = "Selenium Testing"
         searchBox.sendKeys("Selenium Testing");
         searchBox.sendKeys(Keys.ENTER);
+    }
+    public static void testSelenium (WebDriver driver){
+        driver.get("https://www.selenium.dev/");
+        WebElement searchBox = driver.findElement(By.cssSelector("#main_navbar > ul > li:nth-child(7) > a > span"));
+        searchBox.click();
+        searchBox= driver.findElement(By.id("m-blog2016-li"));
+        searchBox.click();
+searchBox= driver.findElement(By.cssSelector("body > div > div.td-main > div > main > div:nth-child(1) > div > ul > li:nth-child(4) > div > h5 > a"));
+        searchBox.click();
     }
     public static WebDriver firefoxDriver() {
         WebDriverManager.firefoxdriver().setup();
